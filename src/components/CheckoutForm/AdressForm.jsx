dec74c074f66adab8f50bcfa473bb9490c6f892e
Marwen.eshili <marweneshili@takiacademyteam.com>
2023-03-14 22:53:40 +0100

    fixing checkout form

--- src/components/CheckoutForm/AdressForm.jsx
+++ src/components/CheckoutForm/AdressForm.jsx
@@ -1,23 +1,91 @@
-import React from 'react'
-import { InputLabel, Select,MenuItem,Button,Grid,Typography } from '@material-ui/core'
-import {useForm , FormProvider} from 'react-hook-form';
-import FormInput from './CustomTextField';
+import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+// import { useForm, FormProvider } from "react-hook-form";
+// import FormInput from "./CustomTextField";
+import { Input, Button } from "antd";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
+const AdressForm = ({ setActiveStep }) => {
+  // const methods = useForm();
 
-const AdressForm = () => {
-  const methods = useForm();
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("First name is required"),
+    lastName: Yup.string().required("Last name is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      //here the values
+      console.log(values);
+      setActiveStep(1);
+    },
+  });
+
   return (
     <>
-      <Typography variant='h6' gutterBottom> Shipping Address</Typography>
-      <FormProvider {...methods}>
-        <form onSubmit="">
-           <Grid container spacing={3}>
-             <FormInput required name='firstName' label='First name'/>
-           </Grid>
-        </form>
-      </FormProvider>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <form onSubmit={formik.handleSubmit} className="form_checkout">
+        {/* <FormInput required name="firstName" label="First name" /> */}
+        <Input
+          placeholder="First name"
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+          onBlur={formik.handleBlur}
+        />
+        {/* error message  */}
+        <p className="error_msg">
+          {formik.touched.firstName && formik.errors.firstName}
+        </p>
+        <Input
+          placeholder="Last name"
+          id="lastname"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+          onBlur={formik.handleBlur}
+        />
+        <p className="error_msg">
+          {formik.touched.lastName && formik.errors.lastName}
+        </p>
+        <Input
+          placeholder="Email"
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+        />
+        <p className="error_msg">
+          {formik.touched.email && formik.errors.email}
+        </p>
+        <Button onClick={formik.handleSubmit}>Submit</Button>
+      </form>
     </>
   );
-}
+};
 
-export default AdressForm
+export default AdressForm;
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,51 +1,52 @@
-import React , {useState}from 'react'
-import {Paper,Stepper , Step , StepLabel,Typography,CircularProgress,Divider,Button} from '@material-ui/core'
-import useStyle from './styles';
-import AdressForm from '../AdressForm';
-import PaymentForm from '../PaymentForm';
-
-const steps =['shipping address', 'Payment details'];
-
+import React, { useState } from "react";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import useStyle from "./styles";
+import AdressForm from "../AdressForm";
+import PaymentForm from "../PaymentForm";
+import "./Checkout.css";
+const steps = ["shipping address", "Payment details"];
 
 const Checkout = () => {
-  const [activeStep , setActiveStep]= useState(0);
-  const classes= useStyle();
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyle();
 
-  const Confirmation = () => (
-    <div>
-      Confirmation
-    </div>
-  ); 
-  
-  const Form = ()=> activeStep === 0 
-    ? <AdressForm/> 
-    : <PaymentForm/>
+  // const Confirmation = () => <div>Confirmation</div>;
+
+  // const Form = ()=> activeStep === 0
+  //   ? <AdressForm/>
+  //   : <PaymentForm/>
 
   return (
     <>
-      <div className={classes.toolbar}/>
+      <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography variant='h4' align='center'>
+          <Typography variant="h4" align="center">
             Checkout
           </Typography>
-          <Stepper activeStep={useState} className={classes.stepper}>
-            {steps.map((step)=>(
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
               <Step key={step}>
-                <StepLabel>
-                  {step}
-                </StepLabel>
+                <StepLabel>{step}</StepLabel>
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.length ? <Confirmation/> : <Form/> }
+          {activeStep === steps.length && " <Confirmation /> "}
+          {activeStep === 0 && <AdressForm setActiveStep={setActiveStep} />}
+          {activeStep === 1 && <PaymentForm setActiveStep={setActiveStep} />}
         </Paper>
       </main>
-
-
     </>
-  )
-}
+  );
+};
 
-export default Checkout
-
+export default Checkout;
